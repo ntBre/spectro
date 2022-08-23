e24d0c185aa88ae485d9f53ec3eeeaa71a7a72df
Brent Westbrook <brentrwestbrook@gmail.com>
2022-08-23 07:41:04 -0400

    move utils tests back into one file

--- src/utils.rs
+++ src/utils.rs
@@ -12,9 +12,6 @@ use crate::{
     Dmat, Dvec, Spectro, Vec3, FACT3, FACT4, FUNIT3, FUNIT4, ICTOP, IPTOC, WAVE,
 };
 
-#[cfg(test)]
-mod tests;
-
 impl Display for Spectro {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use crate::Curvil::*;
@@ -725,4 +722,54 @@ pub(crate) fn princ_cart(irep: usize) -> ([usize; 3], [usize; 3]) {
     let ic = [IPTOC[(irep, 0)], IPTOC[(irep, 1)], IPTOC[(irep, 2)]];
     let id = [ICTOP[(irep, 0)], ICTOP[(irep, 1)], ICTOP[(irep, 2)]];
     (ic, id)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use approx::assert_abs_diff_eq;
+    use nalgebra::dmatrix;
+
+    use crate::FACT2;
+
+    #[test]
+    fn test_find3r() {
+        let got = find3r(2, 2, 2);
+        let want = 9;
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn test_quartic_sum_facs() {
+        let got = quartic_sum_facs(15, 3);
+        let want: Vec<f64> = vec![
+            24.0, 6.0, 4.0, 6.0, 24.0, 6.0, 2.0, 2.0, 6.0, 4.0, 2.0, 4.0, 6.0,
+            6.0, 24.0,
+        ];
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn test_taupcm() {
+        let s = Spectro::load("testfiles/h2o.in");
+        let fc2 = load_fc2("testfiles/fort.15", s.n3n);
+        let fc2 = s.rot2nd(fc2, s.axes);
+        let fc2 = FACT2 * fc2;
+        let w = s.geom.weights();
+        let sqm: Vec<_> = w.iter().map(|w| 1.0 / w.sqrt()).collect();
+        let fxm = s.form_sec(fc2, s.n3n, &sqm);
+        let (harms, lxm) = symm_eigen_decomp(fxm);
+        let freq = to_wavenumbers(harms);
+        let (_zmat, _biga, wila) = s.zeta(s.natom, s.nvib, &lxm, &w);
+        let primat = s.geom.principal_moments();
+        let tau = make_tau(3, 3, &freq, &primat, &wila);
+        let got = tau_prime(3, &tau);
+        let want = dmatrix![
+        -0.08628870,  0.01018052, -0.00283749;
+         0.01018052, -0.00839612, -0.00138895;
+        -0.00283749, -0.00138895, -0.00093412;
+           ];
+        assert_abs_diff_eq!(got, want, epsilon = 2e-7);
+    }
 }
