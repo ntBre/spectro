8fd8ac2d07d9d1f254068a1aef9a2ad20a09e6d6
Brent Westbrook <brentrwestbrook@gmail.com>
2022-08-26 10:51:08 -0400

    subdivide test module and clean up run tests

---
+++ src/tests/load.rs
@@ -0,0 +1,138 @@
+use std::str::FromStr;
+
+use crate::Curvil::*;
+use crate::*;
+
+use na::matrix;
+use nalgebra as na;
+
+#[test]
+fn load() {
+    let got = Spectro::load("testfiles/spectro.in");
+    let want = Spectro {
+        header: vec![
+            1, 1, 5, 2, 0, 0, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ],
+        geom: Molecule::from_str(
+            "
+C      0.00000000 -0.89784221  0.00000000
+C      0.67631628  0.36939882  0.00000000
+C     -0.67631628  0.36939882  0.00000000
+H      1.55759109  0.94685817  0.00000000
+H     -1.55759109  0.94685817  0.00000000
+",
+        )
+        .unwrap(),
+        weights: vec![
+            (1, 12.0),
+            (2, 12.0),
+            (3, 12.0),
+            (4, 1.007825),
+            (5, 1.007825),
+        ],
+        curvils: vec![
+            Bond(2, 3),
+            Bond(1, 2),
+            Bond(1, 3),
+            Bond(2, 4),
+            Bond(3, 5),
+            Bend(2, 4, 1),
+            Bend(3, 5, 1),
+            Tors(4, 2, 1, 3),
+            Tors(5, 3, 1, 2),
+        ],
+        degmodes: vec![],
+        dummies: vec![],
+        rotor: Rotor::AsymmTop,
+        n3n: 15,
+        i3n3n: 680,
+        i4n3n: 3060,
+        nvib: 9,
+        i2vib: 45,
+        i3vib: 165,
+        i4vib: 495,
+        natom: 5,
+        axes: matrix![
+        1.0, 0.0, 0.0;
+        0.0, 1.0, 0.0;
+        0.0, 0.0, 1.0;
+        ],
+    };
+    assert_eq!(got, want);
+}
+
+#[test]
+fn load_dummy() {
+    let got = Spectro::load("testfiles/dummy.in");
+    let want = Spectro {
+        header: vec![
+            1, 1, 3, 0, 2, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ],
+        geom: Molecule::from_str(
+            "
+He    -1.76798258  0.00000000  0.00000000
+H     -0.53078563  0.00000000  0.00000000
+H      0.53078563  0.00000000  0.00000000
+He     1.76798258  0.00000000  0.00000000
+",
+        )
+        .unwrap(),
+        weights: vec![
+            (1, 4.00260325413),
+            (2, 1.007825),
+            (3, 1.007825),
+            (4, 4.00260325413),
+            (5, 0.00),
+            (6, 0.00),
+            (7, 0.00),
+            (8, 0.00),
+        ],
+        curvils: vec![
+            Bond(1, 2),
+            Bond(2, 3),
+            Bond(3, 4),
+            Bend(2, 1, 3),
+            Bend(3, 2, 4),
+        ],
+        degmodes: vec![vec![3, 2, 0], vec![1, 2, 3], vec![4, 6], vec![5, 7]],
+        dummies: vec![
+            Dummy {
+                x: DummyVal::Value(1.1111111111),
+                y: DummyVal::Atom(3),
+                z: DummyVal::Atom(1),
+            },
+            Dummy {
+                x: DummyVal::Atom(3),
+                y: DummyVal::Value(1.1111111111),
+                z: DummyVal::Atom(1),
+            },
+            Dummy {
+                x: DummyVal::Value(1.1111111111),
+                y: DummyVal::Atom(3),
+                z: DummyVal::Atom(2),
+            },
+            Dummy {
+                y: DummyVal::Value(1.1111111111),
+                x: DummyVal::Atom(3),
+                z: DummyVal::Atom(2),
+            },
+        ],
+        rotor: Rotor::Linear,
+        n3n: 12,
+        i3n3n: 364,
+        i4n3n: 1365,
+        nvib: 7,
+        i2vib: 28,
+        i3vib: 84,
+        i4vib: 210,
+        natom: 4,
+        axes: matrix![
+        0.0, 0.0, 1.0;
+        1.0, 0.0, 0.0;
+        0.0, 1.0, 0.0;
+        ],
+    };
+    assert_eq!(got, want);
+}
---
+++ src/tests/run.rs
@@ -0,0 +1,144 @@
+use std::{fs::read_to_string, path::PathBuf};
+
+use approx::{abs_diff_eq, assert_abs_diff_eq};
+use serde::Deserialize;
+
+use crate::*;
+
+use na::DVector;
+use nalgebra as na;
+
+fn load_want(filename: PathBuf) -> Output {
+    #[derive(Clone, Deserialize, Debug)]
+    #[allow(unused)]
+    struct Want {
+        harms: Vec<f64>,
+        funds: Vec<f64>,
+        corrs: Vec<f64>,
+        rots: Vec<Vec<f64>>,
+    }
+    let data = read_to_string(filename).unwrap();
+    let want: Want = serde_json::from_str(&data).unwrap();
+    let mut rots = vec![Rot::new(
+        vec![0; want.harms.len()],
+        want.rots[0][2],
+        want.rots[0][0],
+        want.rots[0][1],
+    )];
+    for i in 0..want.harms.len() {
+        let mut tmp = vec![0; want.harms.len()];
+        tmp[i] = 1;
+        rots.push(Rot::new(
+            tmp,
+            want.rots[i + 1][2],
+            want.rots[i + 1][0],
+            want.rots[i + 1][1],
+        ));
+    }
+    Output {
+        harms: Dvec::from(want.harms),
+        funds: want.funds,
+        corrs: want.corrs,
+        rots,
+    }
+}
+
+#[derive(Clone)]
+struct Test {
+    infile: PathBuf,
+    fort15: PathBuf,
+    fort30: PathBuf,
+    fort40: PathBuf,
+    want: Output,
+}
+
+impl Test {
+    fn new(dir: &'static str) -> Self {
+        let start = Path::new("testfiles");
+        Self {
+            infile: start.join(dir).join("spectro.in"),
+            fort15: start.join(dir).join("fort.15"),
+            fort30: start.join(dir).join("fort.30"),
+            fort40: start.join(dir).join("fort.40"),
+            want: load_want(start.join(dir).join("summary.json")),
+        }
+    }
+}
+
+fn check(got: Vec<f64>, want: Vec<f64>, msg: &str, label: &str) {
+    assert!(got.len() == want.len());
+    if !abs_diff_eq!(
+        Dvec::from(got.clone()),
+        Dvec::from(want.clone()),
+        epsilon = 0.1
+    ) {
+        println!("\n{:>5}{:>8}{:>8}{:>8}", "Mode", "Got", "Want", "Diff");
+        for i in 0..got.len() {
+            println!(
+                "{:5}{:8.1}{:8.1}{:8.1}",
+                i + 1,
+                got[i],
+                want[i],
+                got[i] - want[i],
+            );
+        }
+        assert!(false, "{} differ at {}", msg, label);
+    }
+}
+
+fn check_rots(got: Vec<Rot>, want: Vec<Rot>, infile: &str) {
+    assert_eq!(got.len(), want.len());
+    if !abs_diff_eq!(
+        DVector::from(got.clone()),
+        DVector::from(want.clone()),
+        epsilon = 3e-5
+    ) {
+        println!("{}", "got");
+        for g in &got {
+            println!("{}", g);
+        }
+        println!("{}", "want");
+        for g in &want {
+            println!("{}", g);
+        }
+        println!("{}", "diff");
+        for g in 0..got.len() {
+            println!(
+                "{}",
+                Rot::new(
+                    got[g].state.clone(),
+                    got[g].a - want[g].a,
+                    got[g].b - want[g].b,
+                    got[g].c - want[g].c,
+                )
+            );
+        }
+        assert!(false, "rots differ at {}", infile);
+    }
+}
+
+#[test]
+fn test_run() {
+    let tests = [
+        Test::new("h2o"),
+        Test::new("h2co"),
+        Test::new("c3h2"),
+        Test::new("c2h4"),
+        Test::new("allyl"),
+        Test::new("h2o_sic"),
+        // Test::new("c3hf"),
+        // Test::new("c3hcl"),
+        // Test::new("c3hcn010"),
+        // Test::new("c3hcn"),
+    ];
+    for test in Vec::from(&tests[..]) {
+        let infile = test.infile.to_str().unwrap();
+        let spectro = Spectro::load(infile);
+        let got = spectro.run(test.fort15, test.fort30, test.fort40);
+
+        assert_abs_diff_eq!(got.harms, test.want.harms, epsilon = 0.1);
+        check(got.funds, test.want.funds, "funds", infile);
+        check(got.corrs, test.want.corrs, "corrs", infile);
+        check_rots(got.rots, test.want.rots, infile);
+    }
+}
