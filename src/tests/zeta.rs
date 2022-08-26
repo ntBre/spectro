35bc3d32a72a5013ab0349472843ea819e8b2162
Brent Westbrook <brentrwestbrook@gmail.com>
2022-08-26 11:52:56 -0400

    make zeta test table-based

--- src/tests/zeta.rs
+++ src/tests/zeta.rs
@@ -14,28 +14,54 @@ fn load_dmat(filename: &str, rows: usize, cols: usize) -> Dmat {
     .transpose()
 }
 
+#[derive(Clone)]
+struct Test {
+    infile: String,
+    fort15: String,
+    wila: Dmat,
+    zmat: Tensor3,
+}
+
+impl Test {
+    fn new(dir: &'static str, rows: usize, cols: usize) -> Self {
+        let start = Path::new("testfiles");
+        Self {
+            infile: String::from(
+                start.join(dir).join("spectro.in").to_str().unwrap(),
+            ),
+            fort15: String::from(
+                start.join(dir).join("fort.15").to_str().unwrap(),
+            ),
+            wila: load_dmat(
+                start.join(dir).join("wila").to_str().unwrap(),
+                rows,
+                cols,
+            ),
+            zmat: Tensor3::load(start.join(dir).join("zmat").to_str().unwrap()),
+        }
+    }
+}
+
 #[test]
 fn test_zeta() {
-    let s = Spectro::load("testfiles/h2o/spectro.in");
-    let fc2 = load_fc2("testfiles/fort.15", s.n3n);
-    let fc2 = s.rot2nd(fc2, s.axes);
-    let fc2 = FACT2 * fc2;
-    let w = s.geom.weights();
-    let sqm: Vec<_> = w.iter().map(|w| 1.0 / w.sqrt()).collect();
-    let fxm = s.form_sec(fc2, s.n3n, &sqm);
-    let (_harms, lxm) = symm_eigen_decomp(fxm);
-    // TODO test the other return values
-    let (zmat, _biga, wila) = s.zeta(s.natom, s.nvib, &lxm, &w);
+    let tests = [
+        // had to swap sign of first row of wila since my LXM has a different
+        // sign, and had to negate all of zmat from sign of geometry
+        Test::new("h2o", 3, 6),
+    ];
+    for test in tests {
+        let s = Spectro::load(&test.infile);
+        let fc2 = load_fc2(&test.fort15, s.n3n);
+        let fc2 = s.rot2nd(fc2, s.axes);
+        let fc2 = FACT2 * fc2;
+        let w = s.geom.weights();
+        let sqm: Vec<_> = w.iter().map(|w| 1.0 / w.sqrt()).collect();
+        let fxm = s.form_sec(fc2, s.n3n, &sqm);
+        let (_harms, lxm) = symm_eigen_decomp(fxm);
 
-    // had to swap sign of first row since my LXM has a different sign
-    let want_wila = load_dmat("testfiles/h2o/wila", 3, 6);
+        let (zmat, wila) = s.zeta(&lxm, &w);
 
-    // had to negate all of zmat from sign of geometry
-    let want_zmat = Tensor3::load("testfiles/h2o/zmat");
-
-    println!("{:.8}", wila);
-    println!("{:.8}", want_wila);
-
-    assert_abs_diff_eq!(wila, want_wila, epsilon = 1e-6);
-    assert_abs_diff_eq!(zmat, want_zmat, epsilon = 1e-6);
+        assert_abs_diff_eq!(wila, test.wila, epsilon = 1e-6);
+        assert_abs_diff_eq!(zmat, test.zmat, epsilon = 1e-6);
+    }
 }
