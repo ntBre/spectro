ef1567e4b27789bd38cb7b0395fd616a87e20de7
Brent Westbrook <brentrwestbrook@gmail.com>
2023-02-08 14:52:54 -0600

    actually use finish flag

--- spectro_bin/src/main.rs
+++ spectro_bin/src/main.rs
@@ -1,7 +1,7 @@
-use std::path::Path;
+use std::{error::Error, path::Path};
 
 use clap::Parser;
-use spectro::Spectro;
+use spectro::{Spectro, SpectroFinish};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -19,16 +19,31 @@ struct Args {
     finish: bool,
 }
 
-fn main() -> Result<(), std::io::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     let cfg = Args::parse();
-    let spectro = Spectro::load(&cfg.infile);
-    let infile = Path::new(&cfg.infile);
-    let dir = infile.parent().unwrap_or_else(|| Path::new("."));
-    let (got, _) = spectro.run_files(
-        dir.join("fort.15"),
-        dir.join("fort.30"),
-        dir.join("fort.40"),
-    );
+    let (got, spectro) = if cfg.finish {
+        let SpectroFinish {
+            spectro,
+            freq,
+            f3qcm,
+            f4qcm,
+            irreps,
+            lxm,
+            lx,
+        } = SpectroFinish::load(&cfg.infile)?;
+        let (g, _) = spectro.finish(freq, f3qcm, f4qcm, irreps, lxm, lx);
+        (g, spectro)
+    } else {
+        let spectro = Spectro::load(&cfg.infile);
+        let infile = Path::new(&cfg.infile);
+        let dir = infile.parent().unwrap_or_else(|| Path::new("."));
+        let (g, _) = spectro.run_files(
+            dir.join("fort.15"),
+            dir.join("fort.30"),
+            dir.join("fort.40"),
+        );
+        (g, spectro)
+    };
     if cfg.json {
         let data = serde_json::to_string_pretty(&got)?;
         println!("{data}");
