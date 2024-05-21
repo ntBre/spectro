de8cde4558da0561acb9eee9cee6575a04687974
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-21 17:30:21 -0400

    add to_json and to_json_pretty convenience functions for Output

--- spectro/src/output.rs
+++ spectro/src/output.rs
@@ -142,3 +142,13 @@ impl Display for Output {
         Ok(())
     }
 }
+
+impl Output {
+    pub fn to_json(&self) -> Result<String, serde_json::Error> {
+        serde_json::to_string(&self)
+    }
+
+    pub fn to_json_pretty(&self) -> Result<String, serde_json::Error> {
+        serde_json::to_string_pretty(&self)
+    }
+}
